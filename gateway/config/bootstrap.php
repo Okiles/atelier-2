fd729e2a7409308b226283b7a138eace3d93bc1a
Okiles <colsonlenny5@gmail.com>
2025-02-03 09:42:21 +0100

    adding cors middleware

--- gateway/config/bootstrap.php
+++ gateway/config/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
 
 use DI\ContainerBuilder;
+use geoquizz\gateway\application\middlewares\CorsMiddleware;
+use slim\Psr7\Request;
 use Slim\Factory\AppFactory;
+use Slim\Psr7\Response;
 
 $builder = new ContainerBuilder();
 $builder->addDefinitions(__DIR__ . '/settings.php' );
@@ -9,12 +12,12 @@ $builder->addDefinitions(__DIR__ . '/dependencies.php');
 
 $c = $builder->build();
 $app = AppFactory::createFromContainer($c);
-/*
+
 $app->add(new CorsMiddleware());
 $app->options('/{routes:.+}',
     function(Request $rq, Response $rs, array $args) : Response {
         return $rs;
-    });*/
+    });
 
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
