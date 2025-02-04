dff86d0c1101e30d2f3a3e18143e1193c01cab9d
Okiles <colsonlenny5@gmail.com>
2025-02-04 10:49:06 +0100

    Updating registration

--- app-game/src/applications/infrastructure/repositories/UserRepository.php
+++ app-game/src/applications/infrastructure/repositories/UserRepository.php
@@ -4,17 +4,16 @@ namespace geoquizz\game\applications\infrastructure\repositories;
 
 use geoquizz\game\applications\core\domain\entities\User\User;
 use geoquizz\game\applications\core\repositoryInterfaces\RepositoryEntityNotFoundException;
-use geoquizz\game\applications\core\repositoryInterfaces\UserRepositoryInterface;
 use PDO;
 
-class UserRepository implements UserRepositoryInterface
+class Userrepository
 {
     private PDO $pdo;
     public function __construct(PDO $pdo){
         $this->pdo = $pdo;
     }
 
-    public function getUser(string $id) : User {
+    public function getUser(string $id){
         $query = $this->pdo->prepare("SELECT * FROM `users` WHERE `id` = :id");
         $query->execute(['id' => $id]);
 
@@ -29,36 +28,4 @@ class UserRepository implements UserRepositoryInterface
     }
 
 
-    public function createUser(User $user): void
-    {
-        $this->pdo->prepare("INSERT INTO `users` (`id`, `name`, `lastname`, `email`, `username`, 'profilepic') VALUES (:id, :name, :lastname, :email, :username, :profilepic)")
-            ->execute([
-                'id' => $user->getId(),
-                'name' => $user->getName(),
-                'lastname' => $user->getLastname(),
-                'email' => $user->getEmail(),
-                'username' => $user->getUsername(),
-                'profilepic' => $user->getProfilePic()
-            ]);
-    }
-
-    public function save(User $user): void
-    {
-        $this->pdo->prepare("UPDATE `users` SET `name` = :name, `lastname` = :lastname, `email` = :email, `username` = :username, `profilepic` = :profilepic WHERE `id` = :id")
-            ->execute([
-                'id' => $user->getId(),
-                'name' => $user->getName(),
-                'lastname' => $user->getLastname(),
-                'email' => $user->getEmail(),
-                'username' => $user->getUsername(),
-                'profilepic' => $user->getProfilePic()
-            ]);
-    }
-
-    public function deleteUser(string $id): bool
-    {
-        $query = $this->pdo->prepare("DELETE FROM `users` WHERE `id` = :id");
-        $query->execute(['id' => $id]);
-        return $query->rowCount() > 0;
-    }
 }
