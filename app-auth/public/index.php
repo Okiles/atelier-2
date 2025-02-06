c9ccc77e114c1d6f054d1021d133fc644994413c
Okiles <colsonlenny5@gmail.com>
2025-02-06 10:41:06 +0100

    fixed register

--- app-auth/public/index.php
+++ app-auth/public/index.php
@@ -1,12 +1,7 @@
 <?php
 declare(strict_types=1);
 
-use Dotenv\Dotenv;
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../config');
-$dotenv->load();
 
 /* application boostrap */
 $appli = require_once __DIR__ . '/../config/bootstrap.php';
